feffe7179ca86bdadab7225fdfb11f5d66996c64
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-04-10 02:26:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (75.42%) - LeetHub

--- 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
+++ 1638-count-substrings-that-differ-by-one-character/1638-count-substrings-that-differ-by-one-character.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    int countSubstrings(string s, string t) 
-    {
-        int res=0;
-        int n=s.size(),m=t.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(s[i]!=t[j])
-                {
-                    int l=1,r=1;
-                    while(min(i-l,j-l)>=0&&s[i-l]==t[j-l])
-                        l++;
-                    while(i+r<n&&j+r<m&&s[i+r]==t[j+r])
-                        r++;
-                    res+=l*r;
+    int countSubstrings(string s, string t) {
+        int n = s.size(), m = t.size(), ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(s[i] != t[j]){
+                    int cnt1 = 0, cnt2 = 0;
+                    int ips = i - 1, ipt = j - 1, ifs = i + 1, ift = j + 1;
+                    while(ips >= 0 && ipt >= 0 && s[ips] == t[ipt]){
+                        cnt1++, ips--, ipt--;
+                    }
+                    while(ifs < n && ift < m && s[ifs] == t[ift]){
+                        cnt2++, ifs++, ift++;
+                    }
+                    ans += cnt1 * cnt2 + cnt1 + cnt2 + 1;
                 }
             }
         }
-        return res;
-        
+        return ans;
     }
 };
