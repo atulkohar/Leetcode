438368db3deb9d1742543a34d3fcc536a96163a3
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-04-09 21:28:19 +0530

    Time: 3 ms (39.88%), Space: 6.4 MB (24.81%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    
-    int dp[102][10002];
-    int helper(string s, string t, int n, int m)
-    {
-        if(n==0 or m == 0)
-            return 0;
-        if(dp[n-1][m-1] != -1)
-            return dp[n-1][m-1];
-        if(s[n-1] == t[m-1])
-            return dp[n-1][m-1] = 1 + helper(s,t,n-1,m-1);
-        else
-            return dp[n-1][m-1] = max(helper(s,t,n-1,m),helper(s,t,n,m-1));
-    }
-    
     bool isSubsequence(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return s.size() == helper(s,t,s.size(),t.size());
+        int n1 = s.size(), n2 = t.size();
+        int i = 0, j = 0, ctr = 0;
+        while(i < n1 and j < n2){
+            if(s[i] == t[j]){
+                ctr++;
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(ctr == n1){
+            return true;
+        }
+        return false;
     }
 };
