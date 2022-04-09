3b7c7a26852eba17b5a42fb5577fd9ab3af5557d
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-04-09 20:12:45 +0530

    Time: 7 ms (11.40%), Space: 5.9 MB (70.56%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,9 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    bool divisorGame(int n) {
-        if(n&1)
-            return false;
+    int dp[10001];
+    int helper(int n)
+    {
+        if (n == 1)
+            return 0;
+        if (dp[n] != -1)
+        {
+            return dp[n];
+        }
         else
-            return true;
+        {
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    if (helper(n - i) == 0)
+                        return dp[n] = 1;
+                }
+            }
+            return dp[n] = 0;
+        }
+    }
+    bool divisorGame(int n)
+    {
+        memset(dp, -1, sizeof dp);
+        return helper(n);
     }
 };
