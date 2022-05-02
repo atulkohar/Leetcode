3c847bd74d2c229384b5a0bdfe5bc51f955256df
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-05-02 13:36:34 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (83.77%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
-    vector<int> targetIndices(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        vector<int> v;
-        for(int i=0; i<nums.size(); i++)
-            if(nums[i] == target)
-                v.push_back(i);
+    bool isGood_f(vector<int>&nums,int mid,int x)
+    {
+        if(nums[mid]>=x) return true;
+        else return false;
+    }
+    bool isGood_r(vector<int>&nums,int mid,int x)
+    {
+        if(nums[mid]<=x) return true;
+        else return false;
+    }
+    vector<int> targetIndices(vector<int>& nums, int x) {
+        sort(nums.begin(),nums.end());
+        if(x<nums[0]||x>nums[nums.size()-1]) return {};
+        if(nums.size()==1)
+        {
+            if(nums[0]==x) return {0};
+            else return {};
+        }
+        int l=-1,r=nums.size();
+        while(l+1<r)
+        {
+            int mid=(l+(r-l)/2);
+            if(isGood_f(nums,mid,x)) r=mid;
+            else l=mid;
+        }
+        int lf=-1,rg=nums.size();
+        while(lf+1<rg) 
+        {
+            int mid=(lf+(rg-lf)/2);
+            if(isGood_r(nums,mid,x)) lf=mid;
+            else rg=mid;
+        }
+        if(nums[r]!=x&&nums[lf]!=x) return {};
+        vector<int>v;
+        for(int i=r;i<=lf;i++)
+        {
+            v.push_back(i);
+        }
         return v;
     }
 };
