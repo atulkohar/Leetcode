0c8b81ede449cd0360ff4b988fd872d1cab97b9a
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-05-10 19:50:57 +0530

    Time: 116 ms (25.68%), Space: 15 MB (63.28%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,15 +1,8 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        visited = {}
-        def dp(point):
-            if(point[0]>=len(triangle)):
-                return 0
-            x,y=point[0],point[1]
-            if(str([x,y]) in  visited):
-                return visited[str([x,y])]
-            else:
-                visited[str([x,y])] = min(triangle[x][y] + dp([x+1,y]),triangle[x][y] + dp([x+1,y+1]))
-                return visited[str([x,y])]
-             
-        
-        return min(triangle[0][0] + dp([1,0]),triangle[0][0] + dp([1,1]))
+        dp = [0] * (len(triangle) + 1)
+        for row in triangle[::-1]:
+            for i,n in enumerate(row):
+                dp[i] = n + min(dp[i], dp[i+1])
+        return dp[0]
+                
