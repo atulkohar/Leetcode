32f5ae6c8b8c93840f8c1aa30197d994ef4ab9fe
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-05-24 19:21:44 +0530

    Time: 533 ms (38.98%), Space: 14.3 MB (49.65%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,15 +1,27 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         dp = {}
-        def dfs(i, a):
-            if a == amount:
-                return 1
-            if a > amount:
-                return 0
-            if i == len(coins):
-                return 0
-            if (i,a) in dp:
-                return dp[(i,a)]
-            dp[(i,a)] = dfs(i, a+coins[i]) + dfs(i + 1, a)
-            return dp[(i,a)]
-        return dfs(0,0)
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+            
+            for a in range(1, amount + 1):
+                nextDP[a] =  dp[a]
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]] 
+            dp = nextDP
+        return dp[amount]
+        # def dfs(i, a):
+        #     if a == amount:
+        #         return 1
+        #     if a > amount:
+        #         return 0
+        #     if i == len(coins):
+        #         return 0
+        #     if (i,a) in dp:
+        #         return dp[(i,a)]
+        #     dp[(i,a)] = dfs(i, a+coins[i]) + dfs(i + 1, a)
+        #     return dp[(i,a)]
+        # return dfs(0,0)
