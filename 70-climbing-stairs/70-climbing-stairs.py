28bd1a30348939383f90580b15698cbe044ffe95
atul-7 <63926842+atul-7@users.noreply.github.com>
2022-06-16 17:31:36 +0530

    Time: 51 ms (27.77%), Space: 14 MB (11.22%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,8 +1,22 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         one, two = 1, 1
+#         for i in range(n-1):
+#             temp = one
+#             one = one+two
+#             two = temp
+#         return one
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n-1):
-            temp = one
-            one = one+two
-            two = temp
-        return one
+        dp = {}
+        def help(i, n):
+            if i == n:
+                return 1
+            if i > n:
+                return 0
+            if (i, n) in dp:
+                return dp[(i,n)]
+            dp[(i,n)] = help(i+1, n) + help(i+2, n)
+            return dp[(i,n)]
+        return help(0, n)
